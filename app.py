3a0b8f99c59678e6c5aefbd12d9aa9dc48726d01
sefatuter <sefa2233_18@hotmail.com>
2024-06-25 02:06:16 +0300

    Major changes

--- app.py
+++ app.py
@@ -5,55 +5,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = db.Column(db.String(80), nullable=False)
-    surname = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
-    school = db.Column(db.String(100), nullable=False)
-    department = db.Column(db.String(100), nullable=False)
-
 @app.route('/')
 def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template('index.html')
 
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    if request.method == 'POST':
-        name = request.form['name']
-        surname = request.form['surname']
-        email = request.form['email']
-        phone = request.form['phone']
-        school = request.form['school']
-        department = request.form['department']
-        new_user = User(name=name, surname=surname, email=email, phone=phone, school=school, department=department)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('add.html')
 
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    user = User.query.get_or_404(id)
-    if request.method == 'POST':
-        user.name = request.form['name']
-        user.surname = request.form['surname']
-        user.email = request.form['email']
-        user.phone = request.form['phone']
-        user.school = request.form['school']
-        user.department = request.form['department']
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('edit.html', user=user)
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    user = User.query.get_or_404(id)
-    db.session.delete(user)
-    db.session.commit()
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     with app.app_context():
